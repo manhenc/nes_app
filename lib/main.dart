c0671d39cba4ab4f4fe63e2e04a94578de5d8e69
ongjiayuan <ongjy98@outlook.com>
2021-01-25 01:34:03 +0800

    Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/login_screen.dart';
 import 'package:flutter_app/screens/MentorListScreen.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MentorListScreen(),
+      home: LoginScreen(),
     );
   }
 }
