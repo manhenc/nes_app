0d0603bc6affe4677992315534b213b84f59cea1
manhenc <60025452+manhenc@users.noreply.github.com>
2021-05-19 16:23:44 +0800

    Create edurec_screen.dart

---
+++ lib/screens/edurec_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EduRecPage extends StatefulWidget {
+  static String id = 'home_page';
+  EduRecPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _EduRecPageState createState() => _EduRecPageState();
+}
+
+class _EduRecPageState extends State<EduRecPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
